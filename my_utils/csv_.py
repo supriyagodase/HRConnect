3c5627fb91e6d7abea7c3d553e0f1a5b273168d8
supriyagodase <supriyagodase19@gmail.com>
2022-12-07 08:10:00 +0530

    commit

--- my_utils/csv_.py
+++ my_utils/csv_.py
@@ -20,14 +20,25 @@ class HandleCSV():
     def read_as_dict(cls):
         with open(cls.filename) as csvfile:
             data=csv.DictReader(csvfile)
-            new_dict={}
-            list=[]
+            emp_list=[]
             for i in data:
+                new_dict = {}
                 for key,val in i.items():
+                    if key in ['FIRST_NAME', 'LAST_NAME', 'PHONE_NUMBER', 'SALARY']:
+                        # print(key, val)
+                        if key == 'PHONE_NUMBER':
+                            new_dict[key] =val.replace('.', '')
+                        else:
+                            new_dict[key]= val
                     # if 'SALARY' >int(9000):
-                    new_dict[key]=val
-                    list.append(new_dict)
-            return list
+                    # new_dict[key]=val
+                    # list.append(new_dict)
+                # print(new_dict)
+                if int(new_dict['SALARY']) > 9000:
+                    emp_list.append(new_dict)
+            # print(len(emp_list))
+            return emp_list
+
     def filter_data(list,):
         for i in list:
             print(i[7])
@@ -38,15 +49,16 @@ class HandleCSV():
 
 
 
-# if __name__ == "__main__" :
-#     obj = HandleCSV()
-#     obj1= obj.read_entire_csv()
-#     # print(obj1)
-#     # print(type(obj1))
-#     obj2 = obj.read_csv_line_by_line()
-#     print(next(obj2))
-#     # for i in obj2:
-#     #     print(next(obj2))
+if __name__ == "__main__" :
+    obj = HandleCSV()
+    # obj1= obj.read_entire_csv()
+    # print(obj1)
+    # print(type(obj1))
+    # obj2 = obj.read_csv_line_by_line()
+    print(obj.read_as_dict())
+    # print(next(obj2))
+    # for i in obj2:
+    #     print(next(obj2))
 
 
 
